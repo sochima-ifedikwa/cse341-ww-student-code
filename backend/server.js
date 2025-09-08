9a4e38bc53062d28f8189e76e00d2b99e15b51ca
sochima-ifedikwa <ifedikwasochima@gmail.com>
2025-09-07 21:20:55 -0500

    Refactor server setup: reorganize middleware usage and update CORS headers

--- backend/server.js
+++ backend/server.js
@@ -2,21 +2,22 @@ const express = require('express'); // Import Express framework
 const bodyParser = require('body-parser'); // Import body-parser middleware
 const professionalRoutes = require('./routes/professional'); // Import professional routes
 
-const app = express(); // Create an Express application
 
-app.use('/professional', professionalRoutes); // Use the professional routes
+const app = express(); // Create an Express application
 app.use(bodyParser.json()); // Use body-parser middleware to parse JSON requests
 
 // Middleware to handle CORS (Cross-Origin Resource Sharing)
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   next();
 });
 
 
+app.use('/professional', professionalRoutes); // Use the professional routes
+
+
 // Start the server
-const port = process.env.PORT || 8080;
+const port = 8080;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
