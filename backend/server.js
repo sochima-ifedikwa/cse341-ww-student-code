468ccc3daef8bc0132064dfb4f29252d658f02eb
sochima-ifedikwa <ifedikwasochima@gmail.com>
2025-09-10 22:16:43 -0500

    Enhance server setup: add MongoDB connection and update server initialization; create user profile JSON

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express'); // Import Express framework
 const bodyParser = require('body-parser'); // Import body-parser middleware
 const professionalRoutes = require('./routes/professional'); // Import professional routes
+const mongodb = require('./database/connect') // Import MongoDB connection module
 
 
 const app = express(); // Create an Express application
@@ -14,10 +15,15 @@ app.use((req, res, next) => {
 
 
 app.use('/professional', professionalRoutes); // Use the professional routes
+const port = process.env.PORT || 8080; // Define the port to run the server on
+// Connect to MongoDB
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port, () => {
+      console.log(`Server is running on port: ${port}`);
+    });
+  }
 
-
-// Start the server
-const port = 8080;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
 });
